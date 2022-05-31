72151e3caaebf5492450e2163d99d24cef3aa684
拔土豆的程序员 <lizhifuabc@163.com>
2022-05-31 11:58:37 +0800

    redis 基础配置

--- tomato-components/tomato-component-redis-starter/src/main/java/com/tomato/redis/RedisConfig.java
+++ tomato-components/tomato-component-redis-starter/src/main/java/com/tomato/redis/RedisConfig.java
@@ -20,21 +20,24 @@ public class RedisConfig {
     public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory lettuceConnectionFactory) {
 
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        // 配置连接工厂
         redisTemplate.setConnectionFactory(lettuceConnectionFactory);
 
         // 用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值
         StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
-        // key采用String的序列化方式
+        // 使用StringRedisSerializer来序列化和反序列化redis的key值
         redisTemplate.setKeySerializer(stringRedisSerializer);
+        // 使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值（默认使用JDK的序列化方式）
         Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
         ObjectMapper objectMapper = new ObjectMapper();
-        // 指定要序列化的域(field,get,set)，访问修饰符(public,private,protected)
+        // 指定要序列化的域(field,get,set),以及修饰符范围，ANY是都有包括private和public
         objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        // 指定序列化输入的类型，类必须是非final修饰的，final修饰的类，比如String,Integer等会跑出异常
         objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL);
         jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
-        // value
+        // 值采用json序列化
         redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
-
+        // 设置hash key 和value序列化模式
         redisTemplate.setHashKeySerializer(stringRedisSerializer);
         redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
 
