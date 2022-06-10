f7f9dfd544cc388dd227ec722a371d3c43e78672
拔土豆的程序员 <lizhifuabc@163.com>
2022-06-10 10:35:50 +0800

    网关增加限流模块

---
+++ tomato-gateway/src/main/java/com/tomato/gateway/config/KeyResolverConfig.java
@@ -0,0 +1,33 @@
+package com.tomato.gateway.config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+/**
+ * KeyResolver
+ *
+ * @author lizhifu
+ * @date 2022/6/10
+ */
+@Configuration
+public class KeyResolverConfig {
+    @Bean
+    public KeyResolver apiKeyResolver () {
+        // 按URL限流,即以每秒内请求数按URL分组统计，超出限流的url请求都将返回429状态
+        return exchange -> Mono.just(exchange.getRequest().getPath().toString());
+    }
+
+    // @Bean
+    public KeyResolver userKeyResolver () {
+        // 按用户限流
+        return exchange -> Mono.just(exchange.getRequest().getQueryParams().getFirst("user"));
+    }
+
+    // @Bean
+    public KeyResolver ipKeyResolver () {
+        // 按IP来限流
+        return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getHostName());
+    }
+}
