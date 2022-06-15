360468522d0bbaf3b3a50ac4fe3edf4eac75d12c
拔土豆的程序员 <lizhifuabc@163.com>
2022-06-15 17:50:19 +0800

    目录结构调整

--- doc/sql/init.sql
+++ doc/sql/init.sql
@@ -1,7 +1,10 @@
+-- 创建用户 root 1qaz@WSX
+create user 'tomato'@'%' identified by 'tomato';
+grant all privileges on *.* to 'tomato'@'%';
+flush privileges;
+-- DATABASE
+CREATE DATABASE IF NOT EXISTS nacos DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_general_ci;
 
--- ----------------------------
---
--- ----------------------------
 CREATE DATABASE IF NOT EXISTS tomato_id DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_general_ci;
 
 CREATE DATABASE IF NOT EXISTS tomato_skill DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_general_ci;
