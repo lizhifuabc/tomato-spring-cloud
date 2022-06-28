579afe753df7e94448a37bf3560d5ce5048f0687
拔土豆的程序员 <lizhifuabc@163.com>
2022-06-28 18:31:49 +0800

    订单查询接口

---
+++ tomato-module-order/order-stater/src/main/java/com/tomato/order/controller/OrderQueryController.java
@@ -0,0 +1,14 @@
+package com.tomato.order.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 订单查询接口
+ * TODO 分库分表下怎么查询
+ * @author lizhifu
+ * @date 2022/6/28
+ */
+@RestController
+public class OrderQueryController {
+
+}
