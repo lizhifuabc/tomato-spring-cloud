9fd02aeafa7e7317deaf2569560c2db1c42357e5
拔土豆的程序员 <lizhifuabc@163.com>
2022-07-05 13:45:50 +0800

    Snowflake

--- tomato-components/tomato-component-utils/src/main/java/com/tomato/utils/date/SystemClock.java
+++ tomato-components/tomato-component-utils/src/main/java/com/tomato/utils/date/SystemClock.java
@@ -11,8 +11,8 @@ import java.util.concurrent.TimeUnit;
  * System.currentTimeMillis()之所以慢是因为去跟系统打了一次交道
  * 后台定时更新时钟，JVM退出时，线程自动回收
  *
- * see： http://git.oschina.net/yu120/sequence
- * @author lry,looly
+ * see： <a href="http://git.oschina.net/yu120/sequence">sequence</a>
+ * @author lry, looly
  */
 public class SystemClock {
 
