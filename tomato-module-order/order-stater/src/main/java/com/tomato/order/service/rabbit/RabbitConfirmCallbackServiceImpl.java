b79b8a7c24049448fa704da320d89a3fde8bd33a
拔土豆的程序员 <lizhifuabc@163.com>
2022-07-12 22:20:55 +0800

    TODO 自定义RabbitMQ消息回调服务实现类

--- tomato-module-order/order-stater/src/main/java/com/tomato/order/service/rabbit/RabbitConfirmCallbackServiceImpl.java
+++ tomato-module-order/order-stater/src/main/java/com/tomato/order/service/rabbit/RabbitConfirmCallbackServiceImpl.java
@@ -1,14 +1,14 @@
-package com.tomato.order.service;
+package com.tomato.order.service.rabbit;
 
 import com.tomato.rabbitmq.service.RabbitConfirmCallbackService;
 import org.springframework.stereotype.Service;
 
 /**
- * Rabbit
- *
+ * 自定义RabbitMQ确认回调服务实现类
+ * TODO
  * @author lizhifu
  * @date 2022/7/12
  */
-//@Service
+@Service
 public class RabbitConfirmCallbackServiceImpl implements RabbitConfirmCallbackService {
 }
---
+++ tomato-module-order/order-stater/src/main/java/com/tomato/order/service/rabbit/RabbitReturnsCallbackServiceImpl.java
@@ -0,0 +1,15 @@
+package com.tomato.order.service.rabbit;
+
+import com.tomato.rabbitmq.service.RabbitReturnsCallbackService;
+import org.springframework.stereotype.Service;
+
+/**
+ * 自定义RabbitMQ消息回调服务实现类
+ * TODO
+ * @author lizhifu
+ * @date 2022/7/12
+ */
+@Service
+public class RabbitReturnsCallbackServiceImpl implements RabbitReturnsCallbackService {
+
+}
