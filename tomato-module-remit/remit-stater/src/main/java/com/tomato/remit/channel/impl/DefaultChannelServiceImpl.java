1e92d0ef4334dac747000aae92e3975076c68364
拔土豆的程序员 <lizhifuabc@163.com>
2022-07-23 13:38:22 +0800

    打款下单

--- tomato-module-remit/remit-stater/src/main/java/com/tomato/remit/channel/impl/DefaultChannelServiceImpl.java
+++ tomato-module-remit/remit-stater/src/main/java/com/tomato/remit/channel/impl/DefaultChannelServiceImpl.java
@@ -3,6 +3,7 @@ package com.tomato.remit.channel.impl;
 import com.tomato.remit.channel.ChannelService;
 import com.tomato.remit.channel.dto.SendRemitResult;
 import com.tomato.remit.dto.RemitOrderReq;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 /**
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
  * @date 2022/7/23
  */
 @Service
+@Slf4j
 public class DefaultChannelServiceImpl implements ChannelService {
 
     @Override
@@ -26,6 +28,11 @@ public class DefaultChannelServiceImpl implements ChannelService {
 
     @Override
     public SendRemitResult remit(RemitOrderReq req) {
-        return null;
+        log.info("默认通道实现，打款下单，req={}", req);
+        SendRemitResult result = new SendRemitResult();
+        result.setRemitStatus(0);
+        result.setExceptionCode("");
+        result.setExceptionInfo("");
+        return result;
     }
 }
